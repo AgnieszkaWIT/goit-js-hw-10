ef78e11bb62d9895519522215958df1dcf5ab6fc
AgnieszkaWIT <151778905+AgnieszkaWIT@users.noreply.github.com>
2024-03-17 06:04:21 +0100

    3333

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,10 @@
-export function fetchCountries(name) {
+const COUNTRIES_URL = "https://restcountries.com/v3.1/name/";
+const COUNTRIES_FIELDS = "?fields=name,capital,population,flags,languages";
 
-}
+export function fetchCountries(name) {
+    fetch(`${COUNTRIES_URL}${name}${COUNTRIES_FIELDS}`)
+        .then(res => {
+            console.log(`${COUNTRIES_URL}${name}${COUNTRIES_FIELDS}`);
+            return res.json();
+        }) 
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import { fetchCountries } from "/src/fetchCountries.js";
+import debounce from "lodash.debounce";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-console.log("test");
+const countrySearchBox = document.getElementById("countrySearchBox");
+const countyInfoData = document.querySelector(".countryInfo");
+
+countrySearchBox.addEventListener('input', debounce(displCounties, 500));
+
+function displCounties(){ 
+    const countryText = countrySearchBox.value.trim();
+    
+    if (countryText === '') {
+        countyInfoData.innerHTML = "";
+        console.log("Pusty_string");
+        return;
+    }
+
+    const zzz = fetchCountries(countryText)
+        //.then(countryData => console.log(countryData))
+    console.log(zzz);
+};
