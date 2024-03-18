d53c0ac3419db0232fba5b2a858215911e9ea05f
AgnieszkaWIT <151778905+AgnieszkaWIT@users.noreply.github.com>
2024-03-18 04:46:22 +0100

    ddd

--- src/index.js
+++ src/index.js
@@ -1,80 +1,84 @@
-import { fetchCountries } from "/src/fetchCountries.js";
-import debounce from "lodash.debounce";
+import { fetchCountries } from '/src/fetchCountries.js';
+import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const countrySearchBox = document.getElementById("countrySearchBox");
-const countyInfoData = document.querySelector(".countryInfo");
-const countriesInfoData = document.querySelector(".countriesInfo");
+const countrySearchBox = document.getElementById('countrySearchBox');
+const countyInfoData = document.querySelector('.countryInfo');
+const countriesInfoData = document.querySelector('.countriesInfo');
 const responseTimeDelay = 300;
 const countriesNumberToBeDispl = 10;
-const emptyString = "";
+const emptyString = '';
 
 countrySearchBox.addEventListener('input', debounce(readCountries, responseTimeDelay));
 
-function readCountries(){ 
-    const countryText = countrySearchBox.value.trim();
-    
-    if (countryText === emptyString) {
+function readCountries() {
+  const countryText = countrySearchBox.value.trim();
+
+  if (countryText === emptyString) {
+    clearData();
+    Notify.info('Empty string entered. Please enter a more specific name.');
+    console.log('Empty string entered');
+    countrySearchBox.value = emptyString;
+    return;
+  }
+
+  fetchCountries(countryText)
+    .then(res => {
+      console.log(res);
+      if (res.length <= countriesNumberToBeDispl) {
         clearData();
-        Notify.info("Empty string entered. Please enter a more specific name.");
-        console.log("Empty string entered");
-        countrySearchBox.value = emptyString;
+        displCountries(res);
+      } else {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        clearData();
         return;
-    }
-    
-    fetchCountries(countryText)
-        .then(res => {
-            console.log(res);
-            if (res.length <= countriesNumberToBeDispl) {
-                clearData();
-                displCountries(res);
-            }
-            else {
-                Notify.info("Too many matches found. Please enter a more specific name.");
-                clearData();
-                return;
-            }
+      }
     })
-        .catch(err => {
-            clearData();
-            Notify.failure("Oops, there is no country with that name");
-        })
-};
-
-const displCountries = res => {
-    if (res.length === 1) {
-        console.log("Single country found");
-        const countryItem = createCountryItem(res);
-        countyInfoData.innerHTML = countryItem;
-    }
-    else {
-        console.log("More than 1 country found");
-        const countryItems = createCountriesItems(res);
-        countriesInfoData.innerHTML = countryItems;
-    }
-
+    .catch(err => {
+      clearData();
+      Notify.failure('Oops, there is no country with that name');
+    });
 }
 
-function clearData () {
-    countyInfoData.innerHTML = emptyString;
-    countriesInfoData.innerHTML = emptyString;
+const displCountries = res => {
+  if (res.length === 1) {
+    console.log('Single country found');
+    const countryItem = createCountryItem(res);
+    countyInfoData.innerHTML = countryItem;
+  } else {
+    console.log('More than 1 country found');
+    const countryItems = createCountriesItems(res);
+    countriesInfoData.innerHTML = countryItems;
+  }
+};
+
+function clearData() {
+  countyInfoData.innerHTML = emptyString;
+  countriesInfoData.innerHTML = emptyString;
 }
 
 const createCountryItem = res => {
-    console.log("Single country added");
-    return res.map(  
-        ({ name, capital, population, flags, languages }) => 
-        `<h1><img src=${flags.png} alt=${name.official} width="60" height="40"> ${name.official}</h1>
+  console.log('Single country added');
+  return res.map(
+    ({ name, capital, population, flags, languages }) =>
+      `<h1><img src=${flags.png} alt=${name.official} width="60" height="40"> ${
+        name.official
+      }</h1>
         <p><strong>Capital:</strong> ${capital}</p>
         <p><strong>Population:</strong> ${population}</p>
         <p><strong>Languages:</strong> ${Object.values(languages)}</p>
         `
-    )};
+  );
+};
 
 const createCountriesItems = res => {
-    console.log("Countries added");
-    return res.map( 
-        ({ name, flags }) => 
+  console.log('Countries added');
+  return res
+    .map(
+      ({ name, flags }) =>
         `<li><img src=${flags.png} alt=${name.official} width="60" height="40"> ${name.official}</li>`
-    ).join(emptyString);
+    )
+    .join(emptyString);
 };
