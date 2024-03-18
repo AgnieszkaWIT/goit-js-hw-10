3f9acc36e6ac0343e826891d0f7bfd78edc92d53
AgnieszkaWIT <151778905+AgnieszkaWIT@users.noreply.github.com>
2024-03-18 02:21:12 +0100

    5555

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,12 @@
-const COUNTRIES_URL = "https://restcountries.com/v3.1/name/";
-const COUNTRIES_FIELDS = "?fields=name,capital,population,flags,languages";
-
 export function fetchCountries(name) {
-    fetch(`${COUNTRIES_URL}${name}${COUNTRIES_FIELDS}`)
-        .then(res => {
-            console.log(`${COUNTRIES_URL}${name}${COUNTRIES_FIELDS}`);
+    const COUNTRIES_URL = "https://restcountries.com/v3.1/name/";
+    const COUNTRIES_FIELDS = "fields=name,capital,population,flags,languages";
+
+    return fetch(`${COUNTRIES_URL}${name}?${COUNTRIES_FIELDS}`).then(res => {
+            //console.log(`${COUNTRIES_URL}${name}?${COUNTRIES_FIELDS}`);
+            if (!res.ok) {
+                throw new Error (res.status);
+            }
             return res.json();
-        }) 
+        })//.then(res => console.log(res)) 
 };
