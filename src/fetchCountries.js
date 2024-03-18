711910673077fff83a61e60cf8991d1b4677578b
AgnieszkaWIT <151778905+AgnieszkaWIT@users.noreply.github.com>
2024-03-18 04:47:06 +0100

    222

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,12 @@
 export function fetchCountries(name) {
-    const COUNTRIES_URL = "https://restcountries.com/v3.1/name/";
-    const COUNTRIES_FIELDS = "fields=name,capital,population,flags,languages";
+  const COUNTRIES_URL = 'https://restcountries.com/v3.1/name/';
+  const COUNTRIES_FIELDS = 'fields=name,capital,population,flags,languages';
 
-    return fetch(`${COUNTRIES_URL}${name}?${COUNTRIES_FIELDS}`).then(res => {
-            //console.log(`${COUNTRIES_URL}${name}?${COUNTRIES_FIELDS}`);
-            if (!res.ok) {
-                throw new Error (res.status);
-            }
-            return res.json();
-        })//.then(res => console.log(res)) 
-};
+  return fetch(`${COUNTRIES_URL}${name}?${COUNTRIES_FIELDS}`).then(res => {
+    //console.log(`${COUNTRIES_URL}${name}?${COUNTRIES_FIELDS}`);
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  }); //.then(res => console.log(res))
+}
